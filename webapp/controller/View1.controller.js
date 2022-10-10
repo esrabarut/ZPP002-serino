69bc85a5b3115dfe827c3187d84363639d0c88d7
ESRABARUT <107683834+esrabarut@users.noreply.github.com>
2022-10-10 14:24:10 +0300

    SON HAL

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -4,7 +4,8 @@ sap.ui.define([
 	"sap/m/MessageToast",
 	"sap/m/MessageBox",
 	"sap/ui/core/UIComponent",
-	"com/ntt/esrabarut/zpp002serino/controller/BaseController"
+	"com/ntt/esrabarut/zpp002serino/controller/BaseController",
+	"com/ntt/esrabarut/zpp002serino/model/models"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
@@ -14,8 +15,9 @@ sap.ui.define([
 		MessageToast,
 		MessageBox,
 		UIComponent,
-		BaseController
-		) {
+		BaseController,
+		models
+	) {
 		"use strict";
 
 		return Controller.extend("com.ntt.esrabarut.zpp002serino.controller.View1", {
@@ -25,14 +27,14 @@ sap.ui.define([
 				let oServiceModel = this.getOwnerComponent().getModel();
 				let oViewModel = this.getOwnerComponent().getModel("QRmodel");  //tırnak içine manifestte tanımladığın json dosyasının adı yazılır. 
 				//barkod işlemleri//
-				// let prefixId = this.createId("_test");
-				// if (prefixId) {
-				// 	prefixId = prefixId.split("--")[0] + "--";
-				// } else {
-				// 	prefixId = "";
-				// }
-				// oScanResultText1 = sap.ui.getCore().byId(prefixId + 'text1');    //1. BARKODUN PREFİX IDSİ
-				// oScanResultText2 = sap.ui.getCore().byId(prefixId + 'text2');    //2. BARKODUN PREFİX IDSİ
+				let prefixId = this.createId("_test");
+				if (prefixId) {
+					prefixId = prefixId.split("--")[0] + "--";
+				} else {
+					prefixId = "";
+				}
+				let oScanResultText1 = sap.ui.getCore().byId(prefixId + 'text1');    //1. BARKODUN PREFİX IDSİ
+				let oScanResultText2 = sap.ui.getCore().byId(prefixId + 'text2');    //2. BARKODUN PREFİX IDSİ
 			},
 			onScanSuccess: function (oEvent) {       //2 PARAMETRE GÖNDERİLECEK.   $EVENT, BB1  İF ELSE
 				var oView = this.getView();
@@ -53,15 +55,15 @@ sap.ui.define([
 				}
 
 			},
-			onScanSuccess2: function (oEvent) {       
+			onScanSuccess2: function (oEvent) {
 				var oView = this.getView();
 				var oServiceModel = this.getOwnerComponent().getModel();
 				var oViewModel = this.getOwnerComponent().getModel("QRmodel");
-				if (oEvent.getParameter("cancelled")) {  
+				if (oEvent.getParameter("cancelled")) {
 					MessageToast.show("Scan cancelled", { duration: 1000 });
 				} else {
-					if (oEvent.getParameter("text")) {   
-						this.getView().byId("text2").setText(oEvent.getParameter("text"));  
+					if (oEvent.getParameter("text")) {
+						this.getView().byId("text2").setText(oEvent.getParameter("text"));
 						this.getView().byId("input2").setValue(oEvent.getParameter("text"));
 						//oViewModel.setProperty("QR2", oScanResultText2);
 					} else {
@@ -77,6 +79,13 @@ sap.ui.define([
 			},
 			onAfterRendering: function (oEvent) {   //2 PARAMETRE GÖNDERİLECEK.   $EVENT, BB1  İF ELSE
 				// Reset the scan result
+			
+				let prefixId = this.createId("_test");
+				if (prefixId) {
+					prefixId = prefixId.split("--")[0] + "--";
+				} else {
+					prefixId = "";
+				}
 				var oScanButton1 = sap.ui.getCore().byId(prefixId + 'sampleBarcodeScannerButton1');  //ndc id
 				var oScanButton2 = sap.ui.getCore().byId(prefixId + 'sampleBarcodeScannerButton2');  //ndc id
 				if (oScanButton1) {
@@ -93,31 +102,49 @@ sap.ui.define([
 				//create işlemi
 				//sonucunda mesaj dönüyorsa yakala.
 
-				
+				//var oViewModel = this.getModel("QRmodel");
 				var oServiceModel = this.getOwnerComponent().getModel();   //burası getview.getmodel olabilir. (DENE)
-	var oData = {};
-				oData.IvBarkod1 = this.getView().byId("input1").getValue();
-				oData.IvBarkod2 = this.getView().byId("input2").getValue();
-				oServiceModel.create("/SeriNoSet", oData, {
+
+				// var oData = {};
+				// oData.IvBarkod1 = this.getView().byId("input1").getValue();
+				// oData.IvBarkod2 = this.getView().byId("input2").getValue();
+
+				let oEntry = {
+					IvBarkod1: this.getView().byId("input1").getValue(),
+					IvBarkod2: this.getView().byId("input2").getValue(),
+					EvMessage: ""
+				}
+
+				// oViewModel.setProperty("/QR1", oData.IvBarkod1);
+				// oViewModel.setProperty("/QR2", oData.IvBarkod2); //oViewModel doldu demektir.
+
+				oServiceModel.create("/SeriNoSet", oEntry, {
 					async: true,
-					success: function (oData) {
-						let exportmessage = oServiceModel.getProperty("/EvMessage");
-						sap.m.MessageBox.success(exportmessage);         //servisten gelen mesaj basılır. Servisten gelen mesajı bir değişkende tutuyoruz, çünkü ekrana basacağız.
+					success: function (oEntry) {
+						debugger
+						//let exportmessage = oServiceModel.getProperty("/EvMessage");
+						//sap.m.MessageBox.success(exportmessage);         //servisten gelen mesaj basılır. Servisten gelen mesajı bir değişkende tutuyoruz, çünkü ekrana basacağız.
+						// var lastmessage= oServiceModel.getProperty("/Ev_Message");
+						// var lastmessage2= oServiceModel.getProperty("/EvMessage");
+						// sap.m.MessageBox.success(lastmessage);
+						// sap.m.MessageBox.success(lastmessage2);
+						// sap.m.MessageBox.success("2.kez geliyor." + oServiceModel.getProperty("/EvMessage"));
+						sap.m.MessageBox.success("Success Message");
 					}.bind(this),
 					error: function (e) {
-						sap.m.MessageBox.error("BARKOD SERVISE ULAŞMADI.");
+						sap.m.MessageBox.error("Error Message");
 						sap.ui.core.BusyIndicator.hide();
 					}
 				});
 
-				this._clearScreen(); //temizliyorum.
+				//this._clearScreen(); //temizliyorum.
 			},
 			_clearScreen: function () {
 				//modeli boşalt
-				that.getView().getModel("oViewModel").clear();      //doldurduğumuz json ımızı temizledik.
-				that.getView().getModel("oViewModel").refresh();	//doldurduğumuz json ımızı temizledik.
+				//this.getView().getModel("oEntry").clear();      //doldurduğumuz json ımızı temizledik.
+				this.getView().getModel("oEntry").refresh();	//doldurduğumuz json ımızı temizledik.
 			}
 
-			
+
 		});
 	});
